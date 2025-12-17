a0b6d7d51c5444301391742d53022c2955e4fe1c
Varma0313 <ravivarmasri13@gmail.com>
2025-12-17 09:02:18 +0530

    Local Storage

--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -9,6 +9,7 @@ import {
   SimpleChanges,
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IToDo } from '../interface/to-do-interface';
 
 @Component({
   selector: 'app-to-do',
@@ -22,12 +23,13 @@ export class ToDoComponent implements OnInit, OnChanges {
   @Output() sendToList = new EventEmitter<any>();
 
   addTask: string = '';
-  toDo: string[] = [];
+  toDo: IToDo[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
     console.log('initlize', 'onChanges');
+    this.getStorage();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
@@ -35,19 +37,51 @@ export class ToDoComponent implements OnInit, OnChanges {
   }
 
   addNewTask() {
-    this.toDo.push(this.addTask);
+    // this.toDo.push(this.addTask);
+    // this.addTask = '';
+    // this.sendToList.emit(this.toDo);
+    // console.log('Check', this.toDo);
+    let toDoItem: IToDo = {
+      task: this.addTask,
+      time: Date.now(),
+      isCompleted: false,
+    };
     this.addTask = '';
-    this.sendToList.emit(this.toDo);
-    console.log('Check', this.toDo);
+    // this.toDo.push(toDoItem);
+    this.toDo = [...this.toDo, toDoItem];
+    this.updateStorage();
   }
 
-  deleteNewTask(index: number) {
-    this.toDo.splice(index, 1);
+  // deleteNewTask(index: number) {
+  //   this.toDo.splice(index, 1);
+  // }
+
+  markCompleted(item: IToDo) {
+    this.toDo = this.toDo.map((toDoItem) => {
+      if (toDoItem.time === item.time) {
+        toDoItem.isCompleted = !toDoItem.isCompleted;
+      }
+      return toDoItem;
+    });
+    this.updateStorage();
   }
+
+  deleteNewTask(item: IToDo) {}
 
   ngOnDestroy(): void {
     this.toDo = [];
     this.addTask = '';
     console.log('Destroy', 'Component is destroyed');
+  }
+
+  getStorage() {
+    try {
+      let storageData: any = localStorage.getItem('my-todo');
+      this.toDo = storageData ? JSON.parse(storageData) || [] : [];
+    } catch (error) {}
+  }
+
+  updateStorage() {
+    localStorage.setItem('my-todo', JSON.stringify(this.toDo));
   }
 }
