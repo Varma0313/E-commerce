3eb9c5f79cd13e1751794ae8767d30f2a3a6c8ae
Varma0313 <ravivarmasri13@gmail.com>
2026-01-20 08:56:52 +0530

    Cosmetic Changes

--- src/app/modules/shopping/component/menu/menu.component.ts
+++ src/app/modules/shopping/component/menu/menu.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { NgFor } from '@angular/common';
+import { ShoppingApiService } from '../services/shopping-api.service';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, NgFor, ProductCardComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  products: any[] = [];
+
+  constructor(private api: ShoppingApiService) {}
+
+  ngOnInit() {
+    this.api.getProducts().subscribe((data) => {
+      this.products = data;
+    });
+  }
   logout() {}
 }
---
+++ src/app/modules/shopping/component/product-card/product-card.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ShoppingApiService } from '../services/shopping-api.service';
+import { SlicePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-product-card',
+  standalone: true,
+  imports: [SlicePipe],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.scss',
+})
+export class ProductCardComponent {
+  products: any[] = [];
+
+  constructor(private api: ShoppingApiService) {}
+
+  ngOnInit(): void {
+    this.api.getProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        console.log('Products from API:', data);
+      },
+      error: (err) => {
+        console.error('Failed to load products', err);
+      },
+    });
+  }
+
+  // 👇 product details from parent
+  @Input() product: any;
+
+  // 👇 quantity
+  quantity: number = 1;
+
+  // 👇 send quantity change to parent if needed
+  @Output() quantityChange = new EventEmitter<number>();
+
+  increaseQty() {
+    this.quantity++;
+    this.quantityChange.emit(this.quantity);
+  }
+
+  decreaseQty() {
+    if (this.quantity > 1) {
+      this.quantity--;
+      this.quantityChange.emit(this.quantity);
+    }
+  }
+}
